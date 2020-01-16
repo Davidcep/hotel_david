7bc2081bad09a75a01349de206e0635cf4efadd0
David <davidcep2018@gmail.com>
2020-01-16 10:17:39 +0100

    template

---
+++ template/master.php
@@ -0,0 +1,17 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/hotel_david/dependencies/templates/ti.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+    <?php startblock('title') ?>
+    <?php endblock() ?>
+    </title>
+</head>
+<body>
+    <?php startblock('content') ?>
+    <?php endblock() ?>
+</body>
+</html>
